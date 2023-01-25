515e7185cfe6d1204853656188d71565289dbac6
Dhanalakshmi P <dhanalakshmi@veniteck.com>
2023-01-25 16:44:20 +0530

    update and add components has modified

--- src/components/pages/Settings/UpdateData.jsx
+++ src/components/pages/Settings/UpdateData.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Paper, Box , Stack, Button, Typography} from "@mui/material";
+import { Paper, Box, Stack, Button, Typography } from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -13,15 +13,15 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { Link, useNavigate, useParams } from "react-router-dom";
 import Switch from '@mui/material/Switch';
 import Swal from "sweetalert2";
-import { getUser, editUser} from "../../../service/api";
+import { getUser, editUser } from "../../../service/api";
 
 const initialValues = {
-  sDescription : '',
-  sOptions : [
+  sDescription: '',
+  sOptions: [
     {
       "text": "",
       "value": "",
-      "isPrompt": ""   
+      "isPrompt": ""
     }
   ]
 }
@@ -34,211 +34,134 @@ export default function UpdateData() {
   const [isAnswer, setIsAnswer] = useState(false);
 
 
+
   useEffect(() => {
-  getUserdata();
-  },[])
+    getUserdata();
+  }, [])
 
   const getUserdata = async () => {
     let response = await getUser(sStatementID);
     setUser(response.data);
   }
-  
-     const onValueChange = (e) => {
-      setUser({ ...user, [e.target.name]: e.target.value})
-      console.log();
-     }
-  
-  
+
+  const onValueChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+
+  }
+
+
   const handleSubmit = async () => {
-  await editUser(user, sStatementID)
-   Swal.fire("Updated !", "Statement has been Updated.", "success")
-   navigate("/Data-table")
+    await editUser(user, sStatementID)
+    Swal.fire("Updated !", "Statement has been Updated.", "success")
+    navigate("/Data-table")
   };
 
   const handleChange = (event) => {
     setIsAnswer(event.target.checked)
-   console.log(event.target.checked);
+    console.log(event.target.checked);
   }
 
 
   return (
 
     <>
-   
-       <div className="bgcolor">
-       <Navbar />
-       <Box height={70} />
-       <Box sx={{ display: "flex" }}>
-         <Sidenav />
-         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-         <Typography variant='h5' align="center" >
-          Update Statement
-          </Typography> 
- 
-           <Paper sx={{ width: "100%", overflow: "hidden" }}>
-             <TableContainer sx={{ maxHeight: 440 }}>
-               <Table stickyHeader aria-label="sticky table">
-                 <TableHead>
-                  {/* <TableHead style={{display:'flex', alignItems:'center'}}>Update Statement</TableHead> */}
-                  
-                   <TableRow>
-                   
-                     {/* <TableCell align="left">Q.No</TableCell> */}
-                     <TableCell align="left">Questions</TableCell>
-                     
-                
-                   </TableRow>
-                 </TableHead>
-                 <TableBody>
-                   
-                     
-                         <TableRow
-                           hover
-                           role="checkbox"
-                           tabIndex={-1}
-                           
-                         >
-                         
-                         <TableCell align="left">  <TextField fullWidth onChange={(e) => onValueChange(e)} name='sDescription' id='outlined basic' variant='standard' size='small' value={user.sDescription} sx={{ minWidth:'auto' }}/>  </TableCell>
-                           {/* <TableCell align="left">  <TextField fullWidth onChange={(e) => onValueChange(e)} name='text' id='outlined basic' variant='standard' size='small' value={user.sOptions[0].text} sx={{ minWidth:'auto' }}/> </TableCell>
-                           <TableCell align="left">  <TextField fullWidth onChange={(e) => onValueChange(e)} name='isPrompt' id='outlined basic' variant='standard' size='small' value={user.sOptions[0].isPrompt} sx={{ minWidth:'auto' }}/>   </TableCell>
-                           <TableCell align="left">    <Switch checked={user.sOptions[0].isAnswer}  onChange={handleChange} name="isAnswer"   value={user.sOptions[0].isAnswer} /> </TableCell>
-                           */}
-                           {/* <TableCell align="left"> 
-                           <Stack spacing={2} direction="row">
-                            <Button
-                            variant="contained" color="success"
-                            onClick={()=>{ handleSubmit()}}
-                            > 
-                                Update 
-                            </Button>
-                           
-                           </Stack>
-                            </TableCell> */}
-                            
-                   </TableRow>
 
-                   <TableHead>
+      <div className="bgcolor">
+        <Navbar />
+        <Box height={70} />
+        <Box sx={{ display: "flex" }}>
+          <Sidenav />
 
-                   <TableCell align="left"> Options</TableCell>
-                     <TableCell align="left"> Prompt</TableCell>
-                     <TableCell align="left"> Answer</TableCell>
-                     <TableCell align="left">Actions</TableCell>
+
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+
+            <Typography variant='h5' align="center" >
+              Update Statement
+            </Typography>
+
+            <Paper sx={{ width: "100%", overflow: "hidden" }}>
+              <TableContainer >
+                <Table stickyHeader aria-label="sticky table">
+                  <TableHead>
+                    {/* <TableHead style={{display:'flex', alignItems:'center'}}>Update Statement</TableHead> */}
+
+                    <TableRow>
+
+                      {/* <TableCell align="left">Q.No</TableCell> */}
+                      <TableCell align="left">Questions</TableCell>
+
+
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+
+
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+
+                    >
+
+                      <TableCell align="left">  <TextField fullWidth onChange={(e) => onValueChange(e)} name='sDescription' id='outlined basic' variant='standard' size='small' value={user.sDescription} sx={{ minWidth: 'auto' }} />  </TableCell>
+      
+
+                    </TableRow>
+
+                    <TableHead>
+
+                      <TableCell align="left"> Options</TableCell>
+                      <TableCell align="left"> Prompt</TableCell>
+                      <TableCell align="left"> Answer</TableCell>
                     
-                   </TableHead>
 
-                   <TableBody>
-                    <TableCell>
-                    <TextField fullWidth onChange={(e) => onValueChange(e)} name='text' id='outlined basic'
-                    //  value={  
-                    //     array.forEach(( user.sOptions, i) => 
-                          
-                         
-                    //       sOptions[i].text[0]
-                         
-                          
-                    //     );
-
-                    //  } 
-                     variant='standard' size='small' sx={{ minWidth:'auto' }}/> <TextField fullWidth onChange={(e) => onValueChange(e)} name='text' id='outlined basic' variant='standard' size='small'  sx={{ minWidth:'auto' }}/> 
-                    </TableCell>
-
-                   </TableBody>
-
-                 
-                   {/* 2nd option */}
-
-                        {/* <TableRow
-                           hover
-                           role="checkbox"
-                           tabIndex={-1}
-                           
-
-                           
-                         >
-                          <TableCell align="left"> <TextField fullWidth id='outlined basic' variant='standard' size='small' sx={{ minWidth:'auto' }} >   </TextField></TableCell>
-                           <TableCell align="left">  <TextField onChange={(e) => onValueChange(e)} name='text' fullWidth id='outlined basic' variant='standard' size='small' value={user.sOptions[1].text} sx={{ minWidth:'auto' }}/> </TableCell>
-                           <TableCell align="left">  <TextField onChange={(e) => onValueChange(e)} name='isPrompt' fullWidth id='outlined basic' variant='standard' size='small' value={user.sOptions[1].isPrompt} sx={{ minWidth:'auto' }}/> </TableCell>
-                           <TableCell align="left">     <Switch checked={user.sOptions[1].isAnswer}   onChange={handleChange} name="isAnswer"  value={user.sOptions[1].isAnswer} /></TableCell>
-                         </TableRow> */}
-
-                         {/* 3rd option */}
-
-                         {/* <TableRow
+                    </TableHead>
+                    
+                    {user && user.sOptions.map((option,i) => {
+                      return(
+                      <>
+                      <TableBody>
+                      <TableRow
                            hover
                            role="checkbox"
                            tabIndex={-1}
                            
                          >
-                          <TableCell align="left"> <TextField fullWidth id='outlined basic' variant='standard' size='small' sx={{ minWidth:'auto' }} >   </TextField></TableCell>
-                           <TableCell align="left">  <TextField onChange={(e) => onValueChange(e)} name='text' fullWidth id='outlined basic' variant='standard' size='small' value={user.sOptions[2].text} sx={{ minWidth:'auto' }}/> </TableCell>
-                           <TableCell align="left">  <TextField onChange={(e) => onValueChange(e)} name='isPrompt' fullWidth id='outlined basic' variant='standard' size='small' value={user.sOptions[2].isPrompt} sx={{ minWidth:'auto' }}/> </TableCell>
-                           <TableCell align="left">     <Switch checked={user.sOptions[2].isAnswer} onChange={handleChange} name="isAnswer"   value={user.sOptions[2].isAnswer} /></TableCell>
+                        <TableCell>
+                          <TextField fullWidth onChange={(e) => onValueChange(e)} name='text' id='outlined basic'
+                            value={option.text}
+                            variant='standard' size='small' sx={{ minWidth: 'auto'  }} />
+                        </TableCell>
 
-                         </TableRow> */}
+                        <TableCell align="left">  <TextField onChange={(e) => onValueChange(e)} name='isPrompt' fullWidth id='outlined basic' variant='standard' size='small' value={option.isPrompt} sx={{ minWidth:'auto' }}/> </TableCell>
+                        <TableCell align="left">     <Switch checked={option.isAnswer}   onChange={handleChange} name="isAnswer"  value={option.isAnswer} /></TableCell>
+                        </TableRow>
+                      </TableBody>
+                      </>
+                      )
+                    })}
 
-                         {/* 4th option */}
-
-                         {/* <TableRow
-                           hover
-                           role="checkbox"
-                           tabIndex={-1}
-                           
-                         >
-                          <TableCell align="left"> <TextField fullWidth id='outlined basic' variant='standard' size='small' sx={{ minWidth:'auto' }} >   </TextField></TableCell>
-                           <TableCell align="left">  <TextField onChange={(e) => onValueChange(e)} name='text' fullWidth id='outlined basic' variant='standard' size='small' value={user.sOptions[3].text} sx={{ minWidth:'auto' }}/> </TableCell>
-                           <TableCell align="left">  <TextField onChange={(e) => onValueChange(e)} name='isPrompt' fullWidth id='outlined basic' variant='standard' size='small' value={user.sOptions[3].isPrompt} sx={{ minWidth:'auto' }}/> </TableCell>
-                           <TableCell align="left">     <Switch checked={user.sOptions[3].isAnswer} onChange={handleChange} name="isAnswer"   value={user.sOptions[3].isAnswer} /></TableCell> */}
-
-                         {/* </TableRow> */}
-
-                            {/* 5th option */}
-{/* 
-                            <TableRow
-                           hover
-                           role="checkbox"
-                           tabIndex={-1}
-                           
-                         >
-                          <TableCell align="left"> <TextField fullWidth id='outlined basic' variant='standard' size='small' sx={{ minWidth:'auto' }} >   </TextField></TableCell>
-                           <TableCell align="left">  <TextField onChange={(e) => onValueChange(e)} name='text' fullWidth id='outlined basic' variant='standard' size='small' value={user.sOptions[4].text} sx={{ minWidth:'auto' }}/> </TableCell>
-                           <TableCell align="left">  <TextField onChange={(e) => onValueChange(e)} name='isPrompt' fullWidth id='outlined basic' variant='standard' size='small' value={user.sOptions[4].isPrompt} sx={{ minWidth:'auto' }}/> </TableCell>
+                  </TableBody>
 
 
-                         </TableRow> */}
+                </Table>
+              </TableContainer>
+              <hr/>
+              <Box padding={1} style={{ display: "flex", justifyContent: "space-around" }}>
+                <Link to="/Data-table">  <Button variant="contained" align="center" ><ArrowBackIcon /> </Button></Link>
 
-                            {/* 6th option */}
-
-                            {/* <TableRow
-                           hover
-                           role="checkbox"
-                           tabIndex={-1}
-                           
-                         >
-                          <TableCell align="left"> <TextField fullWidth id='outlined basic' variant='standard' size='small' sx={{ minWidth:'auto' }} >   </TextField></TableCell>
-                           <TableCell align="left">  <TextField onChange={(e) => onValueChange(e)} name='text' fullWidth id='outlined basic' variant='standard' size='small' value={user.sOptions[5].text} sx={{ minWidth:'auto' }}/> </TableCell>
-                           <TableCell align="left">  <TextField onChange={(e) => onValueChange(e)} name='text' fullWidth id='outlined basic' variant='standard' size='small' value={user.sOptions[5].text} sx={{ minWidth:'auto' }}/> </TableCell>
+                <Button variant="contained" color="success" onClick={() => { handleSubmit() }} >Update</Button>
+              </Box>
 
 
-                         </TableRow> */}
-                         
+            </Paper>
 
-                   
-                
-                 <Link to="/Data-table">  <Button variant="contained" align="center"  ><ArrowBackIcon /> </Button></Link>
-                   
-                 </TableBody>
-                 
+          </Box>
 
-               </Table> 
-             </TableContainer>
-             
-           </Paper>
-         </Box>
-       </Box>
-     </div>
-  
-   
+        </Box>
+      </div>
+
+
     </>
   );
 }
